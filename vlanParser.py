211de0d7dcff94b5dd754d485ee74ac522081b35
Andrew <meroman1@asu.edu>
2016-06-08 16:03:59 -0700

    Now parses VLAN file from start to finish, minus adding the upload date

--- vlanParser.py
+++ vlanParser.py
@@ -1,72 +1,101 @@
+###############################################################################
+# vlanParser.py
+#
+# Turns Allnetworks.csv into our lookup file. This description is intentionally
+# vague b/c opsec. Get at me, haxers.
+###############################################################################
+
 import sys
 import csv
 
-def getCidr(ip, subnet):
-	"""
-	Args:
-		String of IP from one IP/Subnet combo
-		String of subnet from one IP/Subnet combo
-	"""
-	masks = [
-		'0.0.0.0',
-		'128.0.0.0',
-		'192.0.0.0',
-		'224.0.0.0',
-		'240.0.0.0',
-		'248.0.0.0',
-		'252.0.0.0',
-		'254.0.0.0',
-		'255.0.0.0',
-		'255.128.0.0',
-		'255.192.0.0',
-		'255.224.0.0',
-		'255.240.0.0',
-		'255.248.0.0',
-		'255.252.0.0',
-		'255.254.0.0',
-		'255.255.0.0',
-		'255.255.128.0',
-		'255.255.192.0',
-		'255.255.224.0',
-		'255.255.240.0',
-		'255.255.248.0',
-		'255.255.252.0',
-		'255.255.254.0',
-		'255.255.255.0',
-		'255.255.255.128',
-		'255.255.255.192',
-		'255.255.255.224',
-		'255.255.255.240',
-		'255.255.255.248',
-		'255.255.255.252',
-		'255.255.255.254',
-		'255.255.255.255'
-		]
-	cidrs = ['/0','/1','/2','/3','/4','/5','/6','/7','/8','/9',
-		'/10','/11','/12','/13','/14','/15','/16','/17','/18',
-		'/19','/20','/21','/22','/23','/24','/25','/26','/27',
-		'/28','/29','/30','/31','/32']
+def getCidrForSubnet(subnet_mask):
+    masks = [
+        '0.0.0.0',
+        '128.0.0.0',
+        '192.0.0.0',
+        '224.0.0.0',
+        '240.0.0.0',
+        '248.0.0.0',
+        '252.0.0.0',
+        '254.0.0.0',
+        '255.0.0.0',
+        '255.128.0.0',
+        '255.192.0.0',
+        '255.224.0.0',
+        '255.240.0.0',
+        '255.248.0.0',
+        '255.252.0.0',
+        '255.254.0.0',
+        '255.255.0.0',
+        '255.255.128.0',
+        '255.255.192.0',
+        '255.255.224.0',
+        '255.255.240.0',
+        '255.255.248.0',
+        '255.255.252.0',
+        '255.255.254.0',
+        '255.255.255.0',
+        '255.255.255.128',
+        '255.255.255.192',
+        '255.255.255.224',
+        '255.255.255.240',
+        '255.255.255.248',
+        '255.255.255.252',
+        '255.255.255.254',
+        '255.255.255.255'
+        ]
+    cidrs = ['/0','/1','/2','/3','/4','/5','/6','/7','/8','/9',
+        '/10','/11','/12','/13','/14','/15','/16','/17','/18',
+        '/19','/20','/21','/22','/23','/24','/25','/26','/27',
+        '/28','/29','/30','/31','/32']
 
-	index = masks.index(subnet)
-	return cidrs[index]
+    if '.' not in subnet_mask: # some rows already have CIDR instead of a subnet mask
+        cidr = '/' + subnet_mask
+        return cidr
+    else:
+        index = masks.index(subnet_mask)
+        cidr = cidrs[index]
+        return cidr
+
+def createCsvWithHeaders(outfile_path, header_list):
+    outfile = open(outfile_path, 'a')
+    writer = csv.writer(outfile)
+    writer.writerow(header_list)
+    return writer
+
+def deleteAndsFrom(dept_code):
+    dept_code = dept_code.replace(' and', ',')
+    return dept_code
+
+def writeCondensedInfobloxRowWith(ip_address, subnet_mask, dept_code, comment, outfile_writer):
+    cidr = getCidrForSubnet(subnet_mask)
+    ip_with_cidr = ip_address + cidr
+    dept_code = deleteAndsFrom(dept_code)
+    outfile_writer.writerow([ip_with_cidr, dept_code, comment])
 
 def main():
-	infile_path = '/Users/greatscott/Downloads/Allnetworks.csv'
-	outfile_path = '/Users/greatscott/Downloads/vlan_andrew.csv'
+    infile_path = '/Users/greatscott/Downloads/Allnetworks.csv'
+    outfile_path = '/Users/greatscott/Downloads/vlan_andrew.csv'
 
-	# create CSV that VLAN data will be written to
-	outfile = open(outfile_path, 'a')
-	writer = csv.writer(outfile)
-	writer.writerow(['subnet','dept_code','upload_date']) # write headers
+    outfile_writer = createCsvWithHeaders(outfile_path, ['subnet','dept_code','comments','upload_date'])
+    infile = open(infile_path, 'rU')
+    infile_reader = csv.reader(infile)
 
-	infile = open(infile_path, 'rU')
-	reader = csv.reader(infile)
-	for row in reader:
-		if('255' in row[1]):
-		    subnet = row[0] + getCidr(row[0], row[1])
-		else:
-			subnet = row[0] + '/' + row[1]
-		writer.writerow([subnet, row[2]])
+    network_column_index = 0
+    ip_column_index = 1
+    subnet_column_index = 2
+    comment_column_index = 8
+    dept_code_column_index = 46
+
+    for row in infile_reader:
+        if 'header' in row[network_column_index]:
+            pass
+        else:
+            ip_address = row[ip_column_index]
+            subnet_mask = row[subnet_column_index]
+            comment = row[comment_column_index]
+            dept_code = row[dept_code_column_index]
+            writeCondensedInfobloxRowWith(ip_address, subnet_mask, dept_code, comment, outfile_writer)
 
 if __name__ == '__main__':
-	main()
+    main()
